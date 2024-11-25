a49ecf0545b38fd7ae37e7c09248dfe0b0c50bfa
link5669 <acqmiles@gmail.com>
2024-11-24 22:08:33 -0500

    .env

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,5 @@
 import express from "express";
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import firebaseConfig from "./firebase-config.js";
 import registerPassRoute from "./routes/registerPassRoute.js"
 import getPassRoute from "./routes/getPassRoute.js";
 import filteredPassRoute from "./routes/filteredPassRoute.js";
@@ -10,6 +8,16 @@ import registerConflictRoute from "./routes/registerConflictRoute.js";
 import searchConflicts from "./routes/searchConflicts.js";
 import setCustomLimitRoute from "./routes/setCustomLimitRoute.js";
 import searchLimits from "./routes/getLimitsRoute.js";
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
+}
 
 const port = 5001;
 const app = express();
